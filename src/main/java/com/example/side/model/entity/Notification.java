02fe93934f75de9fdfb36bb53f88bd8b5bd6b1a5
psg <1999byh@naver.com>
2024-05-21 18:46:07 +0900

    Notification 엔티티 생성

--- src/main/java/com/example/side/model/entity/Notification.java
+++ src/main/java/com/example/side/model/entity/Notification.java
@@ -1,2 +1,35 @@
-package com.example.side.model.entity;public class Notification {
+package com.example.side.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    private User user;
+
+    private String title;
+    private String description;
+
+    @CreatedDate
+    private LocalDateTime Created;
+    @LastModifiedDate
+    private LocalDateTime Updated;
+
 }
