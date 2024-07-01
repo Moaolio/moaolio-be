91c17f83d81d8266fa2cdee7bde0f85255629975
psg <1999byh@naver.com>
2024-07-01 15:51:55 +0900

    Feat#4-Comments

--- src/main/java/com/example/side/config/UserDetailsServiceImpl.java
+++ src/main/java/com/example/side/config/UserDetailsServiceImpl.java
@@ -2,15 +2,16 @@ package com.example.side.config;
 
 import com.example.side.model.entity.User;
 import com.example.side.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
-    @Autowired
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
--- src/main/java/com/example/side/controller/CommentsController.java
+++ src/main/java/com/example/side/controller/CommentsController.java
@@ -1,9 +1,12 @@
 package com.example.side.controller;
 
+import com.example.side.config.UserDetailsImpl;
+import com.example.side.request.CommentsRequest;
 import com.example.side.response.CommentsResponse;
 import com.example.side.service.CommentsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +20,19 @@ public class CommentsController {
 
     // 생성
     @PostMapping("/post")
-    public CommentsResponse createComments
+    public CommentsResponse createComments(@RequestBody CommentsRequest commentsRequest, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentsService.createComments(commentsRequest, userDetails);
+    }
 
+    // 수정
     @PutMapping("/update/{commentsId}")
-    public CommentsResponse updateComments
+    public CommentsResponse updateComments(@PathVariable Long commentsId, @RequestBody CommentsRequest commentsRequest, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentsService.updateComments(commentsId, commentsRequest, userDetails);
+    }
 
+    // 삭제
     @DeleteMapping("/delete/{commentsId}")
-    public void deleteComments
+    public void deleteComments(@PathVariable Long commentsId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        commentsService.deleteComments(commentsId, userDetails);
+    }
 }
