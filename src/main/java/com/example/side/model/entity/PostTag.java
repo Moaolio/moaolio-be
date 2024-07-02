83d720ec6e80e2816e4ad761126be0e5738fb46e
psg <1999byh@naver.com>
2024-07-02 14:47:53 +0900

    PostTag NoArgsConstructor 삭제

--- src/main/java/com/example/side/model/entity/PostTag.java
+++ src/main/java/com/example/side/model/entity/PostTag.java
@@ -3,10 +3,13 @@ package com.example.side.model.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class PostTag {
+
     private final String name;
+
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
@@ -21,7 +24,4 @@ public class PostTag {
     @JoinColumn(name = "tag_id")
     private Tag tag;
 
-    public PostTag(String name) {
-        this.name = name;
-    }
 }
