b7201ab4cbbe5aafdea7f946591d8f9625598d50
psg <1999byh@naver.com>
2024-07-18 14:04:26 +0900

    [FEAT]: Jasypt 암호화 적용

--- src/main/java/com/example/side/auth/SecurityConfig.java
+++ src/main/java/com/example/side/auth/SecurityConfig.java
@@ -1,13 +1,49 @@
 package com.example.side.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private CustomUserDetailsService customUserDetailsService;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .formLogin((auth) -> auth.disable());
+
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        /**
+         * 경로별 인가 작업
+         */
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        /**
+         * 세션 설정 -> STATELESS(무상태)
+         */
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }
