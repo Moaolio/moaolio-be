775df24c3396627c988fdf3b7d70fc1ba757de68
Kim MinSeo <86477146+minseid@users.noreply.github.com>
2024-08-29 11:24:06 +0900

    .

--- src/main/java/com/example/side/user/dto/request/UsernameFindRequest.java
+++ src/main/java/com/example/side/user/dto/request/UsernameFindRequest.java
@@ -1,13 +1,21 @@
 package com.example.side.user.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Builder
 @Getter @Setter
+@AllArgsConstructor
 public class UsernameFindRequest {
 
     private String username;
+    @JsonCreator
+    public UsernameFindRequest(@JsonProperty("username") String username, @JsonProperty("email") String email) {
+        this.username = username;
+    }
 
 }
